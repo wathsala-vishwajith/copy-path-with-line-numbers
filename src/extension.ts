96be3a153d453e022d639ca05673027afe8f0586
‘Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-09-03 00:59:29 +0530

    Add copy the relative path with line numbers

--- src/extension.ts
+++ src/extension.ts
@@ -13,19 +13,49 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('copy-path-with-line-numbers.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Copy Path with Line Numbers!');
-	});
 
-	const disposable2 = vscode.commands.registerCommand('copyRelativePathWithLines.copy', () => {
+	const disposable = vscode.commands.registerCommand('copyRelativePathWithLines.copy', () => {
 		// Implementation for copying relative path with line numbers
-		vscode.window.showInformationMessage('Copying relative path with line numbers...');
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor found.');
+			return;
+		}
+		//extract the relative path of the file in the workspace
+		const relativePath = vscode.workspace.asRelativePath(editor.document.uri, false);
+
+
+		const ranges = formatSelections(editor.selections);
+		const result = `${relativePath}:${ranges.join(',')}`;
+
+		vscode.env.clipboard.writeText(result).then(() => {
+			vscode.window.showInformationMessage(`Copied to clipboard!`);
+		});
 	});
 
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+}
+
+export function formatSelections(selections: readonly vscode.Selection[]): string[] {
+	const parts = selections.map(selection => {
+		const startLine = selection.start.line + 1; // Convert to 1-based line number
+		const endLine = selection.end.line + 1; // Convert to 1-based line number
+
+		//if the selection is till the start of the next line, we should not include that line in the range
+		if (selection.end.character === 0 && startLine < endLine) {
+			return `${startLine}-${endLine - 1}`;
+		}
+
+		if (startLine === endLine) {
+			return `${startLine}`;
+		} else {
+			return `${startLine}-${endLine}`;
+		}
+	});
+
+	//dedupe and sort by starting line
+	const uniqueParts = Array.from(new Set(parts));
+	return uniqueParts.sort((a, b) => parseInt(a) - parseInt(b));
 }
 
 
